9954284ad6408d212c1d613b93c6edbcd503f974
Ad31ide <seanchendu@gmail.com>
2023-08-17 10:46:09 -0500

    jeb

--- public/js/list.js
+++ public/js/list.js
@@ -1,49 +1,34 @@
+const asyncHandler = require('express-async-handler')
+
+const friendlist = asyncHandler(async())
+fetch("mockData.json")
+.then(response => response.json())
+.then(data => {
+    // Data is now available as a JavaScript object
+    displayData(data);
+})
+.catch(error => {
+    console.error("Error fetching data:", error);
+});
+
+
 const friendList = [
-        // Provided friend data
-        // ... (copy the entire provided array here)
-    ];
     
-    const friendListContainer = document.getElementById("friend-list");
-    
-    function createFriendCard(friend) {
-        const friendCard = document.createElement("div");
-        friendCard.classList.add("friend-card");
-    
-        const friendDetails = document.createElement("div");
-        friendDetails.classList.add("friend-details");
-    
-        const friendName = document.createElement("p");
-        friendName.classList.add("friend-name");
-        friendName.textContent = `${friend.first_name} ${friend.last_name}`;
-    
-        const friendEmail = document.createElement("p");
-        friendEmail.classList.add("friend-email");
-        friendEmail.textContent = friend.email;
-    
-        const friendGender = document.createElement("p");
-        friendGender.classList.add("friend-gender");
-        friendGender.textContent = `Gender: ${friend.gender}`;
-    
-        const friendLocation = document.createElement("p");
-        friendLocation.classList.add("friend-location");
-        friendLocation.textContent = `Cruise Location: ${friend.cruise_location}`;
-    
-        friendDetails.appendChild(friendName);
-        friendDetails.appendChild(friendEmail);
-        friendDetails.appendChild(friendGender);
-        friendDetails.appendChild(friendLocation);
-    
-        friendCard.appendChild(friendDetails);
-        return friendCard;
-    }
-    
-    function displayFriendList() {
-        friendListContainer.innerHTML = "";
-        friendList.forEach((friend) => {
-            const friendCard = createFriendCard(friend);
-            friendListContainer.appendChild(friendCard);
-        });
-    }
-    
-    displayFriendList();
-    
+];
+
+// Compile Handlebars template
+const source = document.getElementById("friend-template").innerHTML;
+const template = Handlebars.compile(source);
+
+const friendListContainer = document.getElementById("friend-list");
+
+function displayFriendList() {
+    const context = { friends: friendList.map(friend => ({
+        ...friend,
+        profile_picture: `profile_images/${friend.id}.jpg`
+    })) };
+    const html = template(context);
+    friendListContainer.innerHTML = html;
+}
+
+displayFriendList();
